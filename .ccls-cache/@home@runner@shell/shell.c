7f8026a8cc364e91508ffdedebdf530409604dbd
yelby <89603526+HuskieCat@users.noreply.github.com>
2022-03-06 22:23:54 +0000

    Shell works

--- .ccls-cache/@home@runner@shell/shell.c
+++ .ccls-cache/@home@runner@shell/shell.c
@@ -1,7 +1,65 @@
 #include <stdio.h>
+#include <sys/wait.h> //wait
+#include <unistd.h> //fork, execlp
+#include <stdlib.h>
 
-int main(void) 
-{
-  printf("Hello World\n");
+#define LSH_RL_BUFSIZE 1024
+
+void lsh_loop();
+char *lsh_read_line();
+
+int main(int argc, char** argv) 
+{ 
+  //char *envp[] = {(char*) "PATH=/bin", 0};
+
+  lsh_loop();
+
   return 0;
+}
+
+void lsh_loop()
+{
+  char *line;
+  char **args;
+  int status;
+
+  while(status)
+  {
+    printf("> ");
+    line = lsh_read_line();
+    args = lsh_split_line(line);
+    status = lsh_execute(args);
+
+    free(line);
+    free(args);
+  }
+}
+
+char *lsh_read_line()
+{
+  int bufsize = LSH_RL_BUFSIZE;
+  int position = 0;
+  char *buffer = malloc(sizeof(char) * bufsize);
+  int c;
+
+  if(!buffer)
+  {
+    fprintf(stderr, "lsh: allocation error\n");
+    exit(1);
+  }
+
+  while(1)
+  {
+    c = getchar();
+
+    if(c == EOF || c == '\n')
+    {
+      buffer[position] = '\0';
+      return buffer;
+    }
+    else
+    {
+      buffer[position] = c;
+    }
+  }
 }
--- shell.c
+++ shell.c
@@ -1,72 +1,177 @@
 #include <stdio.h>
+#include <sys/wait.h> //wait
+#include <unistd.h> //fork, execlp
+#include <stdlib.h>
+#include <string.h>
 
-void type_prompt();
-void read_command(char, char);
+#define LSH_RL_BUFSIZE 1024
+#define LSH_TOK_BUFSIZE 64
+#define LSH_TOK_DELIM " \t\r\n\a"
 
-int main(void) 
+void lsh_loop();
+char *lsh_read_line();
+char **lsh_split_line(char *line);
+int lsh_launch(char **args);
+
+int lsh_cd(char **args);
+int lsh_help(char **args);
+int lsh_exit(char **args);
+int lsh_num_builtins();
+int lsh_execute(char **args);
+
+int main(int argc, char** argv) 
 {
-  char cmd[100], command[100], *parameters[20];
-
-  char *envp[] = {(char*) "PATH=/bin", 0};
-  while(1)
-  {
-    type_prompt();
-    read_command(command, parameters);
-    if(fork() == 0) //Child
-    {
-      strcpy(cmd, "/bin/");
-      strcat(cmd, command);
-      execvp(cmd, parameters, envp);
-    } 
-    else //Parent
-    {
-      wait();
-    }
-  }
+  printf(" /\\_/\\\n( o.o )\n > ^ <\n");
+  lsh_loop();
   return 0;
 }
 
-void type_prompt()
+void lsh_loop()
 {
-  static int first_time = 1;
-  if(first_time)
-  {
-    const char* clear_screen = " \e[1;1H\e[2J";
-    write(1, clear_screen, 12);
-    first_time = 0;
-  }
+  char *line;
+  char **args;
+  int status = 1;
 
-  printf("Cat-Shell");
+  while(status)
+  {
+    printf("Tortoiseshell [Bradley Henderson]: ");
+    line = lsh_read_line();
+    args = lsh_split_line(line);
+    status = lsh_execute(args);
+
+    free(line);
+    free(args);
+  }
 }
 
-void read_command(char chmd[], char* par[])
+char *lsh_read_line()
 {
-  char line[1024];
-  int count = 0, i = 0, j = 0;
-  char *array[100], *pch;
+  char *line = NULL;
+  ssize_t bufsize = 0;
 
-  for(;;)
+  if(getline(&line, &bufsize, stdin) == -1)
   {
-    int c = fgetc(stdin);
-    line[count++] = (char) c;
-    if(c == '\n')
-      break;
+    if(feof(stdin))
+      exit(0);
+    else
+    {
+      perror("readline");
+      exit(1);
+    }
   }
 
-  if(count == 1)
-    return;
+  return line;
+}
 
-  pch = strtok(line, " \n");
+char **lsh_split_line(char *line)
+{
+  int bufsize = LSH_TOK_BUFSIZE, position = 0;
+  char **tokens = malloc(bufsize * sizeof(char*));
+  char *token;
 
-  while (pch != NULL)
+  if(!tokens)
   {
-    array[i++] = strdup (pch);
-    pch = strtok(NULL, " \n");
+    fprintf(stderr, "lsh: allocation error\n");
+    exit(1);
   }
 
-  strcpy(cmd, array[0]);
+  token = strtok(line, LSH_TOK_DELIM);
+  while(token != NULL)
+  {
+    tokens[position] = token;
+    position++;
 
-  for(int j = 0; j < i; j++)
-    par[j] = array[j];
-  par[i] = NULL;
+    if(position >= bufsize)
+    {
+      bufsize += LSH_TOK_BUFSIZE;
+      tokens = realloc(tokens, bufsize * sizeof(char*));
+      if(!tokens)
+      {
+        fprintf(stderr, "lsh: allocation error\n");
+        exit(1);
+      }
+    }
+    token = strtok(NULL, LSH_TOK_DELIM);
+  }
+  tokens[position] = NULL;
+  return tokens;
+}
+
+int lsh_launch(char **args)
+{
+  pid_t pid, wpid;
+  int status;
+
+  pid = fork();
+  if(pid == 0)
+  {
+    if(execvp(args[0], args) == -1)
+      perror("lsh");
+    exit(1);
+  }
+  else if(pid < 0)
+    perror("lsh");
+  else
+  {
+    while(!WIFEXITED(status) && !WIFSIGNALED(status))
+      wpid = waitpid(pid, &status, WUNTRACED);
+  }
+  return 1;
+}
+
+char *builtin_str[] = {"cd", "help", "exit"};
+
+int(*builtin_func[]) (char**) = {&lsh_cd, &lsh_help, &lsh_exit};
+
+int lsh_num_builtins()
+{
+  return sizeof(builtin_str) / sizeof(char *);
+}
+
+int lsh_cd(char **args)
+{
+  if(args[1] == NULL)
+  {
+    fprintf(stderr, "lsh: expected argument tp \"cd\"\n");
+  }
+  else
+  {
+    if(chdir(args[1]) != 0)
+    {
+      perror("lsh");
+    }
+  }
+  return 1;
+}
+
+int lsh_help(char **args)
+{
+  int i;
+  printf("Stephen Brennan's LSH\n");
+  printf("Type program names and arguments, and hit enter.\n");
+  printf("The following are built in:\n");
+
+  for(int i = 0; i < lsh_num_builtins(); i++)
+    printf(" %s\n", builtin_str[i]);
+
+  printf("Use the man command for information on other programs.\n");
+  return 1;
+}
+
+int lsh_exit(char **args)
+{
+  return 0;
+}
+
+int lsh_execute(char **args)
+{
+  int i;
+  if(args[0] == NULL)
+    return 1;
+
+  for(i = 0; i < lsh_num_builtins(); i++)
+      if(strcmp(args[0], builtin_str[i]) == 0)
+        return (*builtin_func[i]) (args);
+
+  return lsh_launch(args);
 }
